062e0496fa05ebaa326dd68c5ff2bdcb5fc38a32
thoai240699 <thoai240699@gmail.com>
2025-06-21 10:04:34 +0700

    add componens

---
+++ frontend/src/App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import AddReview from './components/add-review';
+import Login from './components/login';
+import Movie from './components/movie';
+import MoviesList from './components/movies-list';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function App() {
+  const [user, setUser] = React.useState(null);
+  async function login(user = null) {
+    setUser(user);
+  }
+  async function logout() {
+    setUser(null);
+  }
+
+  return (<div className="App">
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar.Brand href="#home">Movie Reviews</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link>
+            <Link to={"/movies"}>Movies</Link>
+          </Nav.Link>
+          <Nav.Link >
+            {
+              user ? (
+                <a onClick={logout} href='#home'>Logout User</a>
+              ) : (
+                <Link to={"/login"}>Login</Link>
+              )
+            }
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+
+    <Switch>
+      <Route exact path={["/", "/movies"]}
+        component={MoviesList}>
+      </Route>
+      <Route path="/movies/:id/review" render={(props) =>
+        <AddReview {...props} user={user} />
+      }>
+      </Route>
+      <Route path="/movies/:id/" render={(props) =>
+        <Movie {...props} user={user} />
+      }>
+      </Route>
+      <Route path="/login" render={(props) =>
+        <Login {...props} login={login} />
+      }>
+      </Route>
+    </Switch>
+  </div>);
+}
+
+export default App;
---
+++ frontend/src/components/movies-list.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MovieList() {
+  return <div className="App">Movie List</div>;
+}
+
+export default MovieList;
