3c90712a45665e0683ef9d55d1aa0b67823de661
QA19 <QA19@boston.vn>
2025-07-18 16:01:12 +0700

    add service movies

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,24 +1,21 @@
-import express from "express";
+import express from 'express'
 
-import MoviesController from "./movies.controller.js";
-import ReviewsController from "./reviews.controller.js";
+import MoviesController from './movies.controller.js'
+import ReviewsController from './reviews.controller.js'
 
 // Định nghĩa các điểm cuối API cho các hoạt động liên quan đến phim
-const router = express.Router();
+const router = express.Router()
 
-router.route("/")
-  .get(MoviesController.apiGetMovies);
+router.route('').get(MoviesController.apiGetMovies)
 
-router.route("/id/:id")
-  .get(MoviesController.apiGetMovieById);
+router.route('/id/:id').get(MoviesController.apiGetMovieById)
 
-router.route("/ratings")
-  .get(MoviesController.apiGetRatings);  
+router.route('/ratings').get(MoviesController.apiGetRatings)
 
 router
-  .route("/review")
+  .route('/review')
   .post(ReviewsController.apiPostReview)
   .put(ReviewsController.apiUpdateReview)
-  .delete(ReviewsController.apiDeleteReview);
+  .delete(ReviewsController.apiDeleteReview)
 
-export default router;
+export default router
--- frontend/src/components/movies-list.js
+++ frontend/src/components/movies-list.js
@@ -1,7 +1,89 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import MovieDataService from '../services/movies';
+import { Link } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card'
 
-function MovieList() {
-  return <div className="App">Movie List</div>;
-}
+const MoviesList = (props) => {
+  const [movies, setMovies] = useState([]);
+  const [searchTitle, setSearchTitle] = useState('');
+  const [searchRating, setSearchRating] = useState('');
+  const [ratings, setRatings] = useState(['All Rating']);
 
-export default MovieList;
+  useEffect(() => {
+    retrieveMovies();
+    retrieveRatings();
+  }, []);
+
+  const retrieveMovies = () => {
+    MovieDataService.getAll()
+      .then((response) => {
+        console.log(response.data);
+        setMovies(response.data.movies);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  const retrieveRatings = () => {
+    MovieDataService.getRatings()
+      .then((response) => {
+        console.log(response.data);
+        setRatings(['All Ratings'].concat(response.data));
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const onChangeSearchTitle = (e) => {
+    const searchTitle = e.target.value;
+    setSearchTitle(searchTitle);
+  };
+  const onChangeSearchRating = (e) => {
+    const searchRating = e.target.value;
+    setSearchRating(searchRating);
+  };
+
+  return (
+    <div className="App">
+      <Container>
+        <Form>
+          <Row>
+            <Col>
+              <Form.Group>
+                <Form.Control
+                  type="text"
+                  placeholder="Search by title"
+                  value={searchTitle}
+                  onChange={onChangeSearchTitle}
+                />
+              </Form.Group>
+              <Button variant="primary" type="button" onClick={findByTitle}>
+                Search
+              </Button>
+            </Col>
+            <Col>
+              <Form.Group>
+                <Form.Control as="select" onChange={onChangeSearchRating}>
+                  {ratings.map((rating) => {
+                    return <option value={rating}>{rating}</option>;
+                  })}
+                </Form.Control>
+              </Form.Group>
+              <Button variant="primary" type="button" onClick={findByRating}>
+                Search
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+    </div>
+  );
+};
+
+export default MoviesList;
